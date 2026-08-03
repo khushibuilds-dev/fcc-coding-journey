e7811a2c6327736afb0bbca0483bc312de32b789
Khushi Singh <khu4656@gmail.com>
2026-08-03 12:43:58 +0530

    Add story generation logic

--- javascript-projects/build-a-sentence-maker/script.js
+++ javascript-projects/build-a-sentence-maker/script.js
@@ -4,3 +4,6 @@ let verb = "eat";
 let place = "Lucknow";
 let adjective2 = "quiet";
 let noun2 = "mango";
+let firstStory = "Once upon a time, there was a(n) " + adjective + " " + noun + " " + "who loved to eat " + noun2 +  ". The " + noun + " " + "lived in a " + place + " " + "and had " + adjective2 + " " + "nostrils that blew fire when it was " + verb + ".";
+console.log("First story: " + firstStory);
+adjective = "boy";
