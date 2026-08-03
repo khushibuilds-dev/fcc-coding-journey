b62592a52dcc50e10b35f8ca4b862cc4b0c25b10
Khushi Singh <khu4656@gmail.com>
2026-08-03 12:48:35 +0530

    complete second story logic and log output

--- javascript-projects/build-a-sentence-maker/script.js
+++ javascript-projects/build-a-sentence-maker/script.js
@@ -12,3 +12,5 @@ verb = "sleep";
 place = "banthara";
 adjective2 = "preety";
 noun2 = "apple";
+let secondStory = "Once upon a time, there was a(n) " + adjective + " " + noun + " " + "who loved to eat " + noun2 +  ". The " + noun + " " + "lived in a " + place + " " + "and had " + adjective2 + " " + "nostrils that blew fire when it was " + verb + ".";
+console.log("Second story: " + secondStory);
