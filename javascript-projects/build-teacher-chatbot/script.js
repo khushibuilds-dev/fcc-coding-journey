13041f406fa6014886180f406c2768d220396cdb
Khushi Singh <khu4656@gmail.com>
2026-08-08 14:14:17 +0530

    Added sentence

--- javascript-projects/build-teacher-chatbot/script.js
+++ javascript-projects/build-teacher-chatbot/script.js
@@ -6,3 +6,6 @@ const subject = "JavaScript";
 const topic = "string";
 const sentence = `Today, you will learn about $(topic) in ${subject}.`;
 console.log(sentence);
+const strLengthIntro = `Here is an example of using the word ${subject}.`;
+console.log(strLengthIntro);
+console.log(subject.length);
