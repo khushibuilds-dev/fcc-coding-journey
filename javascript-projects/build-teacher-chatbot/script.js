1cd732a9244eab5c121cc99217fdcebf88b4e5fe
Khushi Singh <khu4656@gmail.com>
2026-08-08 14:06:21 +0530

    Added a greeting log

---
+++ javascript-projects/build-teacher-chatbot/script.js
@@ -0,0 +1,8 @@
+console.log("Hi there!");
+const botName = "teacherBot";
+const greeting = `My name is ${botName}.`;
+console.log(greeting);
+const subject = "JavaScript";
+const topic = "string";
+const sentence = `Today, you will learn about $(topic) in ${subject}.`;
+console.log(sentence);
