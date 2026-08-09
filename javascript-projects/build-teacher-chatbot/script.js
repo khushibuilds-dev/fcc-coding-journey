70850efb3216e64f0db9b38f650a102f61596c43
Khushi Singh <khu4656@gmail.com>
2026-08-09 11:18:30 +0530

    Added subject

--- javascript-projects/build-teacher-chatbot/script.js
+++ javascript-projects/build-teacher-chatbot/script.js
@@ -11,3 +11,11 @@ console.log(strLengthIntro);
 console.log(subject.length);
 console.log(`Here is an example of using the length property on the word ${topic}.`);
 console.log(topic.length);
+console.log(`Here is an example of accessing the first letter in the word ${subject}.`);
+
+console.log(subject[0]);
+
+console.log(`Here is an example of accessing the second letter in the word ${subject}.`);
+console.log(subject[1]);
+
+console.log(`Here is an example of accessing the last letter in the word ${subject}.`);
