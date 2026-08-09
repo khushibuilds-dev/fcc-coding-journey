830c29ab932cf20d49b886921e757a972eb06613
Khushi Singh <khu4656@gmail.com>
2026-08-09 11:19:56 +0530

    Added fun sentence

--- javascript-projects/build-teacher-chatbot/script.js
+++ javascript-projects/build-teacher-chatbot/script.js
@@ -19,3 +19,15 @@ console.log(`Here is an example of accessing the second letter in the word ${sub
 console.log(subject[1]);
 
 console.log(`Here is an example of accessing the last letter in the word ${subject}.`);
+
+const lastCharacter = subject[subject.length - 1];
+console.log(lastCharacter);
+
+const learningIsFunSentence = "Learning is fun.";
+
+console.log("Here are examples of finding the positions of substrings in the sentence.");
+
+console.log(learningIsFunSentence.indexOf("Learning"));
+
+console.log(learningIsFunSentence.indexOf("fun"));
+console.log(learningIsFunSentence.indexOf("learning"));
