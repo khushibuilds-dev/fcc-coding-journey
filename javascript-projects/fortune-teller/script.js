7f7a2a55ad65ec0625d88e90583d83c5ed150fc6
Khushi Singh <khu4656@gmail.com>
2026-09-03 15:31:11 +0530

    Added fortune teller

--- javascript-projects/fortune-teller/script.js
+++ javascript-projects/fortune-teller/script.js
@@ -1,1 +1,34 @@
-const fortune1 = "Your cat will look very cuddly today.";
+// Hackthon Fortune teller
+function fortuneTeller(name){
+  let fortune = ["ye saal apka bhut achha jayga.",
+    "apko is saal korbaar mai trakki milegi. ❤️",
+    "apko is mahine thoda sambhal kar rahna hoga.",
+    "Apki do mhino mai pooch nikla jayegi nahi to jada sochna band kar do.",
+    "Agr mummy se maar kahana hai to aaj ghr der se jaye."
+  ]
+  let color = ["Red", "Blue", "Black", "Green", "Orange", "Purple"];
+  let colorIndex = Math.floor(Math.random() * color.length);
+  let fortuneIndex = Math.floor(Math.random() * fortune.length);
+  return `Hello ${name}! Ajj apka magal color: ${color[colorIndex]} Ajj apka bhagya: ${fortune[fortuneIndex]}`
+}
+console.log(fortuneTeller("Khuhsi"));
+// Added js into html
+
+const btn = document.getElementById("myBtn");
+const result = document.getElementById("result");
+const nameBox = document.getElementById("input")
+
+
+btn.addEventListener("click", function(){
+
+let enteredName = nameBox.value;
+
+if(enteredName === ""){
+    enteredName = "Stranger";
+}
+
+let report = fortuneTeller(enteredName);
+result.innerText = report;
+
+
+})
