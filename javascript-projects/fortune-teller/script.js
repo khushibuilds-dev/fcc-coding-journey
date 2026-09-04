7e636b0f112596d61ba97eaa1a7e38954f1152fe
Khushi Singh <khu4656@gmail.com>
2026-09-04 15:28:25 +0530

    Tested fortune

--- javascript-projects/fortune-teller/script.js
+++ javascript-projects/fortune-teller/script.js
@@ -1,15 +1,17 @@
 // Hackthon Fortune teller
 function fortuneTeller(name){
-  let fortune = ["ye saal apka bhut achha jayga.",
+  let fortune = ["ye saal apka bhut achha jayga.💕",
     "apko is saal korbaar mai trakki milegi. ❤️",
-    "apko is mahine thoda sambhal kar rahna hoga.",
-    "Apki do mhino mai pooch nikla jayegi nahi to jada sochna band kar do.",
-    "Agr mummy se maar kahana hai to aaj ghr der se jaye."
+    "aap bikul gadhe jaise dhikhte hai.🫏🙉🙈",
+    "Apke pass dimaag ki kami hai.🥴",
+    "Aap bhut hoshiyaar ho or chalak bhi ho.😘"
   ]
   let color = ["Red", "Blue", "Black", "Green", "Orange", "Purple"];
   let colorIndex = Math.floor(Math.random() * color.length);
   let fortuneIndex = Math.floor(Math.random() * fortune.length);
-  return `Hello ${name}! Ajj apka magal color: ${color[colorIndex]} Ajj apka bhagya: ${fortune[fortuneIndex]}`
+  return `Hello ${name}!  
+  Ajj apka lucky color: ${color[colorIndex]} 
+  Ajj apka bhagya: ${fortune[fortuneIndex]}`
 }
 console.log(fortuneTeller("Khuhsi"));
 // Added js into html
